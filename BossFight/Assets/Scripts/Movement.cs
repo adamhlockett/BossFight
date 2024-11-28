ef3049fd0377e63098721fb7836cf69671064ea8
Adam Lockett <adam2.lockett@live.uwe.ac.uk>
2024-11-28 19:48:32 +0000

    Controller movement + tilemap
    Controller movement allows for walk + run animations on the player. Isometric tilemap added to allow for floor texture on the map.

--- BossFight/Assets/Scripts/Movement.cs
+++ BossFight/Assets/Scripts/Movement.cs
@@ -9,7 +9,7 @@ public class Movement : MonoBehaviour
     private Vector2 input;
     Animator anim;
     private Vector2 lastMoveDirection;
-    private bool facingLeft = true;
+    Vector2 moves;
 
     void Start()
     {
@@ -35,19 +35,19 @@ public class Movement : MonoBehaviour
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+        moves = new Vector2(moveX, moveY);
 
         if ((moveX == 0 && moveY == 0) && (input.x != 0 || input.y != 0)) lastMoveDirection = input;
 
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
-        input.Normalize();
     }
 
     void ManageAnims()
     {
         anim.SetFloat("MoveX", input.x);
         anim.SetFloat("MoveY", input.y);
-        anim.SetFloat("MoveMagnitude", input.magnitude);
+        anim.SetFloat("MoveMagnitude", moves.magnitude);
         anim.SetFloat("LastMoveX", lastMoveDirection.x);
         anim.SetFloat("LastMoveY", lastMoveDirection.y);
     }
