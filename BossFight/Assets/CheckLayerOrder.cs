d6f4bf4f4564f1d1e47414a0c5c79c459bbd30f4
Adam Lockett <adam2.lockett@live.uwe.ac.uk>
2025-01-10 00:23:28 +0000

    Fixed Dynamic Sort Order

---
+++ BossFight/Assets/CheckLayerOrder.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckLayerOrder : MonoBehaviour
+{
+    [SerializeField] Transform playerTr;
+    [SerializeField] GameObject enemy;
+    private Transform enemyTr;
+    private SpriteRenderer enemySprite;
+
+    private void Start()
+    {
+        enemyTr = enemy.GetComponent<Transform>();
+        enemySprite = enemy.GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerTr.position.y > enemyTr.position.y)
+        {
+            enemySprite.sortingOrder = 2;
+        }
+        else enemySprite.sortingOrder = 1;
+    }
+}
