3928c2d736a6aeae7b2141a9097e58d684d6664b
Adam Lockett <adam2.lockett@live.uwe.ac.uk>
2025-01-16 13:05:22 +0000

    Fixed aspect ratio issue with Tutorial Video
    Tutorial video was being moved out of position when played in different aspect ratio. Set position to the same as Tutorial Info and added Aspect Ratio Fitter component to fix.

--- BossFight/Assets/Scripts/TestInfoHider.cs
+++ BossFight/Assets/Scripts/TestInfoHider.cs
@@ -10,11 +10,14 @@ public class TestInfoHider : MonoBehaviour
     [SerializeField] RawImage video;
     [SerializeField] GameObject moveTo;
     [SerializeField] GameObject moveFrom;
-    [SerializeField] GameObject videoMoveTo;
-    [SerializeField] GameObject videoMoveFrom;
     [SerializeField] GameObject videoGo;
     public bool hide = false, isWaiting = false;
     public float speed = 5f;
+
+    private void Start()
+    {
+        videoGo.transform.position = moveFrom.transform.position;
+    }
 
     private void Update()
     {
@@ -22,13 +25,15 @@ public class TestInfoHider : MonoBehaviour
         {
             var step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, moveTo.transform.position, step);
-            videoGo.transform.position = Vector3.MoveTowards(videoGo.transform.position, videoMoveTo.transform.position, step);
+            //videoGo.transform.position = Vector3.MoveTowards(videoGo.transform.position, videoMoveTo.transform.position, step * 3.85f);
+            videoGo.transform.position = transform.position;
         }
         else
         {
             var step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, moveFrom.transform.position, step);
-            videoGo.transform.position = Vector3.MoveTowards(videoGo.transform.position, videoMoveFrom.transform.position, step);
+            //videoGo.transform.position = Vector3.MoveTowards(videoGo.transform.position, videoMoveFrom.transform.position, step * 3.85f);
+            videoGo.transform.position = transform.position;
         }
     }
 
